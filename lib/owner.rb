87f2170c2e43a7e32f092c98d7ef71490d4a0fdb
github username <github email address>
2019-02-22 03:43:51 -0800

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,9 +1,25 @@
 class Owner
+  attr_accessor :name, :pets
   attr_reader :species
   @@all = []
 
   def initialize(species)
     @species = species
+    @name = name
+    @pets = {fishes: [], cats: [], dogs: []}
+    @@all << self
+  end
+
+  def say_species
+    "I am a #{@species}."
+  end
+
+  def name
+    @name
+  end
+
+  def pets
+    @pets
   end
 
   def self.all
